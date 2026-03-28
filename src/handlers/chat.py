0194bcffc76c621766d4caea499acd0779e3877a
ayne272 <shanyashka22@gmail.com>
2026-03-28 07:44:31 +0300

    fix(fix): hot fix for fix top dispay at zero

--- src/handlers/chat.py
+++ src/handlers/chat.py
@@ -104,12 +104,11 @@ async def top_handler(message: Message) -> None:
     
         for idx, player in enumerate(top_players):
             medal = medals[idx] if idx < 3 else f" {idx + 1}. "
-            lines.append(
-                f"{medal} {player.first_name} {player.last_name} — "
-            )
             if player.dick == 0:
-                lines.append(f"відвалилася піська")
+                lines.append(
+                    f"{medal} {player.first_name} {player.last_name} — відвалилася піська"
+                )
             else:
-                lines.append(f"{player.dick} см.")
+                lines.append(f"{medal} {player.first_name} {player.last_name} — {player.dick} см.")
         
         await message.answer("\n".join(lines))
