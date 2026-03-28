bda916382105dbd1222a1d54673a2d5fb979521c
ayne272 <shanyashka22@gmail.com>
2026-03-28 08:34:22 +0300

    fix(admin): fix set dick command

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -23,29 +23,27 @@ labeler.auto_rules = [IsBotAdminRule()]
 
 @labeler.message(text="/сброс")
 async def reset_handler(message: Message) -> None:
+    """Сбрасывает кулдаун всем игрокам в текущем чате."""
     async with AsyncSessionLocal() as session:
         stmt = (
             update(Player)
+            .where(Player.chat_id == message.peer_id)
             .values(last_roll_date=None)
             # where(Player.last_roll_date.isnot(None)) # Опционально: можно сбрасывать только у тех, у кого он есть
         )
         result = await session.execute(stmt)
         await session.commit()
 
-    await message.answer(
-        f"✅ Таймеры успешно сброшены!\n"
-        f"🔄 Обновлено профилей: {result.rowcount}\n"
-    )
+    await message.answer(f"Успіх!")
 
-@labeler.message(text="/изменить_писюн <new_dick>")
-async def set_dick_handler(message: Message, new_dick: str) -> None:
+@labeler.message(text="/изменить_писюн <new_dick_str>")
+async def set_dick_handler(message: Message, new_dick_str: str) -> None:
     """Устанавливает игроку новый писюн в текущем чате."""
-
     if not message.reply_message:
         return
         
     try:
-        new_height = int(new_dick)
+        new_dick = int(new_dick_str)
     except ValueError:
         return
 
@@ -56,8 +54,9 @@ async def set_dick_handler(message: Message, new_dick: str) -> None:
         stmt = (
             update(Player)
             .where(Player.vk_id == target_id, Player.chat_id == chat_id)
-            .values(height=new_height)
+            .values(dick=new_dick)
         )
         result = await session.execute(stmt)
         await session.commit()
-            
+
+    await message.answer(f"Успіх!")            
